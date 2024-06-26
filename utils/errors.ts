b0b55dcb827cc39e9026c107680239978bc43852
LukaMazione <luka.mazione@gmail.com>
2024-06-26 23:33:16 +0100

    Added AI reviewer

--- utils/errors.ts
+++ utils/errors.ts
@@ -2,7 +2,12 @@ import { Request, Response, NextFunction } from 'express';
 
 class ValidationError extends Error {}
 
-const handleError = (err: Error, req: Request, res: Response, next: NextFunction) => {
+const handleError = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   console.log(err);
   res.status(err instanceof ValidationError ? 400 : 500).json({
     message: err instanceof ValidationError ? err.message : 'Try agan later',
